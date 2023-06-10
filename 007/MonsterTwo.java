16d88a80d871ca7f33a8ee2a5f7d745634c52c10
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-06-10 11:46:44 +0100

    Add monster movement on the Monster class

--- 007/MonsterTwo.java
+++ 007/MonsterTwo.java
@@ -15,7 +15,7 @@ public class MonsterTwo
     private int movement = 2;
     private boolean alive = true;
 
-    // public variables are visible outside of the class
+    // public variables are visible outside the class
     // You should have as few as possible public fields
     public String name = "Big Monster";
     public char nameChar1 = 'B';
@@ -82,6 +82,15 @@ public class MonsterTwo
         return alive;
     }
 
+    public void setHealth(int decreaseHealth)
+    {
+        health = health - decreaseHealth;
+        if (health < 0)
+        {
+            alive = false;
+        }
+    }
+
     public void setHealth(double decreaseHealth)
     {
         int intDecreaseHealth = (int) decreaseHealth;
@@ -90,6 +99,105 @@ public class MonsterTwo
         if (health < 0)
         {
             alive = false;
+        }
+    }
+
+    public void moveMonster(MonsterTwo[] monsters, int arrayItemIndex) {
+        // isSpaceOpen will be used to track whether the space the
+        // monster plans to move into is occupied
+        boolean isSpaceOpen = true;
+
+        // Define the maximum x and y for the battle board
+        int maxXBoardSpace = battleBoard.length - 1;
+        int maxYBoardSpace = battleBoard[0].length - 1;
+
+        // while loop used to make sure we don't move a monster into an occupied space
+        while (isSpaceOpen) {
+            // Randomly generate move direction N, S, E, or W
+            int randMoveDirection = (int) (Math.random() * 4);
+
+            // Randomly generate move distance based on max move distance (class variable)
+            int randMoveDistance = (int) (Math.random() * (this.getMovement() + 1));
+
+            // Prints move distance and move direction
+            System.out.println(
+                    "Monster: " + this.name + ", " +
+                    "Move Distance: " + randMoveDistance + ", " +
+                    "Move Direction: " + randMoveDirection);
+
+
+            // Erase monsters character on the board by replacing it with a *
+            battleBoard[this.yPosition][this.xPosition] = '*';
+
+            if (randMoveDirection == 0) // N
+            {
+                // Find new xPosition & yPosition based on the current position on the board
+                // If statements won't allow monster to move off the board
+
+                if ((this.yPosition - randMoveDistance) < 0) {
+                    this.yPosition = 0;
+                } else {
+                    this.yPosition = this.yPosition - randMoveDistance;
+                }
+            } else if (randMoveDirection == 1) // E
+            {
+                if ((this.xPosition + randMoveDistance) > maxXBoardSpace) {
+                    this.xPosition = maxXBoardSpace;
+                } else {
+                    this.xPosition = this.xPosition + randMoveDistance;
+                }
+            } else if (randMoveDirection == 2) // S
+            {
+                if ((this.yPosition + randMoveDistance) > maxYBoardSpace) {
+                    this.yPosition = maxYBoardSpace;
+                } else {
+                    this.yPosition = this.yPosition + randMoveDistance;
+                }
+            } else // W
+            {
+                if ((this.xPosition - randMoveDistance) < 0) {
+                    this.xPosition = 0;
+                } else {
+                    this.xPosition = this.xPosition - randMoveDistance;
+                }
+            }
+
+            // monster.length returns the number of items in the array monster
+            for (int i = 0; i < monsters.length; i++) {
+                // If statement skips checking the same monster position against itself
+                if (i == arrayItemIndex) {
+                    continue;
+                }
+
+                if (onMySpace(monsters, i, arrayItemIndex)) {
+                    // If a monster tries to move to an occupied space the
+                    // while loop repeats after break out of the for loop
+                    isSpaceOpen = true;
+                    break;
+                } else {
+                    // There was no monster in the space so end the while loop
+                    isSpaceOpen = false;
+
+                }
+
+            }
+
+        } // End of while loop
+
+        // Set the value in the array to the first letter of the monster
+        battleBoard[this.yPosition][this.xPosition] = this.nameChar1;
+
+    }
+
+    public boolean onMySpace(MonsterTwo[] monsters, int indexToChk1, int indexToChk2)
+    {
+        // Checks if the 2 monsters have the same x && y position
+        if ((monsters[indexToChk1].xPosition) == (monsters[indexToChk2].xPosition) &&
+                (monsters[indexToChk1].yPosition) == (monsters[indexToChk2].yPosition))
+        {
+            return true;
+        } else {
+            return false;
         }
     }
 
@@ -123,6 +231,11 @@ public class MonsterTwo
     public MonsterTwo()
     {
         numOfMonsters++;
+    }
+
+    public static void main(String[] args)
+    {
+
     }
 
 }
