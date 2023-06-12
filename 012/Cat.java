e308986acd77cc2158d417ed0f9a896ca75be6af
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-06-12 22:21:31 +0100

    Add subclass cat used on the polymorphism, inheritance demo

---
+++ 012/Cat.java
@@ -0,0 +1,45 @@
+// Cat is a Subclass of Animal
+// You create subclasses with the extend keyword
+// Now Cat has all the Methods and Fields that Animal defined
+// This is known as inheritance because Cat inherits all
+// the methods and fields defined in Animal
+
+public class Cat extends Animal
+{
+
+    // You can add new fields to the subclass
+    public String favToy = "Yarn";
+
+    public void playWith(){
+
+        System.out.println("Yeah " + favToy);
+    }
+
+    // Here we overrode the Animal walkAround method
+    public void walkAround(){
+
+        // this refers to a specific object created of type Cat
+        System.out.println(this.getName() + " walks aournd and then sleeps");
+
+    }
+
+    public String getToy(){
+
+        return this.favToy;
+    }
+
+    public Cat(){
+
+    }
+
+    public Cat(String name, String favFood, String favToy){
+        // super calls the constructor for the super class Animal
+        super(name, favFood);
+
+        // We set the favToy value in Cat because it doesn't
+        // exist in the Animal class
+        this.favToy = favToy;
+
+    }
+
+}
