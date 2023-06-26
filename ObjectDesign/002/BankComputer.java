defe056341fa6d6f30c2187ef34af3bb8c2450fb
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-06-26 23:29:39 +0100

    Add BankComputer class used on TestATM demo

---
+++ ObjectDesign/002/BankComputer.java
@@ -0,0 +1,14 @@
+public class BankComputer {
+
+    private String name;
+
+    public BankComputer(String name){
+
+        this.name = name;
+    }
+
+    public void addAcctToBank(Account theAccount){
+
+
+    }
+}
