5d078dfe69c31e4e47e5585cf1e16cfd2983df69
Tiago Maia Vieira <Tiago.VM.Vieira@ctw.bmwgroup.com>
2023-06-27 23:42:58 +0100

    Update TestATM interface to finish the demo

--- ObjectDesign/002/TestATM.java
+++ ObjectDesign/002/TestATM.java
@@ -4,11 +4,31 @@ public class TestATM {
 
         Account theAccount = new Account(10, "Tiago Vieira", "C", 10000.00, 1234);
 
-        Card customersATMCard = new Card(Account.getPIN(), theAccount.getStripNumber());
+        Card customersATMCard = new Card(theAccount.getPIN(), theAccount.getStripNumber());
 
         Customer theCustomer = new Customer(customersATMCard);
 
+        BankComputer xyzBankComputer = new BankComputer("XYZ Bank");
 
+        xyzBankComputer.addAcctToBank(theAccount);
+
+        BankNetwork theBankNetwork = new BankNetwork();
+
+        theBankNetwork.addBankToNetwork(xyzBankComputer);
+
+        ATM mainStreetATM = new ATM(theBankNetwork);
+
+        mainStreetATM.isStripReadable(customersATMCard);
+
+        mainStreetATM.isATMCardsBankInNetwork(customersATMCard);
+
+        mainStreetATM.insertPIN(customersATMCard, 1234);
+
+        mainStreetATM.pickAcctToAccess();
+
+        mainStreetATM.amountToWithdrawal();
+
+        mainStreetATM.getTransactionInfo();
 
     }
 }
